2cfc64653682e9f4d7f63b92e0be494abfbaeba6
kzambrana <kzambrana0912@gmail.com>
2025-05-10 12:27:22 -0400

    Init project

---
+++ src/test/java/com/linkify/linkify_be/LinkifyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkify.linkify_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkifyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
